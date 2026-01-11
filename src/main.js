a977a0cc67c6ac054c527aaa6a6c45b4b5f3dc73
Victoria <vitagood.92@list.ru>
2026-01-11 23:54:05 +0300

    Conneted films model

--- src/main.js
+++ src/main.js
@@ -6,16 +6,19 @@ import FilterView from './view/filter-view.js';
 import FooterStatisticsView from './view/footer/footer-statistics-view.js';
 import BoardPresenter from './presenter/board-presenter.js';
 
+import FilmsModel from './model/films-model.js';
+
 const siteHeader = document.querySelector('.header');
 const siteMain = document.querySelector('.main');
-
 const siteFooter = document.querySelector('.footer');
 const footerStatisticsContainer = siteFooter.querySelector('.footer__statistics');
 
+const filmsModel = new FilmsModel();
 const boardPresenter = new BoardPresenter();
+
 
 render(new UserProfileView(), siteHeader);
 render(new FilterView(), siteMain);
 render(new FooterStatisticsView(), footerStatisticsContainer);
 
-boardPresenter.init(siteMain);
+boardPresenter.init(siteMain, filmsModel);
--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -13,16 +13,18 @@ export default class BoardPresenter {
   filmsListComponent = new FilmsListView();
   filmsList = new FilmsListContainerView();
 
-  init = (boardContainer) => {
+  init = (boardContainer, filmsModel) => {
     this.boardContainer = boardContainer;
+    this.filmsModel = filmsModel;
+    this.boardFilms = [...this.filmsModel.getFilms()];
 
     render(new SortView(), this.boardContainer);
     render(this.filmsContainer, this.boardContainer);
     render(this.filmsListComponent, this.filmsContainer.getElement());
     render(this.filmsList, this.filmsListComponent.getElement());
 
-    for (let i = 0; i < 6; i++) {
-      render(new FilmsCardView(), this.filmsList.getElement());
+    for (let i = 0; i < this.boardFilms.length; i++) {
+      render(new FilmsCardView(this.boardFilms[i]), this.filmsList.getElement());
     }
 
     render(new ShowMoreBtnView(), this.boardContainer);
