2bd17cc32c6a2359276e6df3ac4bd6656d99ac41
Victoria <vitagood.92@list.ru>
2026-01-11 23:45:51 +0300

    Added film structure

---
+++ src/mock/comments.js
@@ -0,0 +1,39 @@
+export const commentsArray = [
+  {
+    id: 1,
+    author: 'Ilya O"Reilly',
+    comment: 'a film that changed my life, a true masterpiece, post-credit scene was just amazing omg.',
+    date: '2019-05-11T16:12:32.554Z',
+    emotion: 'smile'
+  },
+  {
+    id: 2,
+    author: 'Ilya O"Reilly',
+    comment: 'a film that changed my life, a true masterpiece, post-credit scene was just amazing omg.',
+    date: '2019-05-11T16:12:32.554Z',
+    emotion: 'sleeping'
+  },
+  {
+    id: 3,
+    author: 'Ilya O"Reilly',
+    comment: 'a film that changed my life, a true masterpiece, post-credit scene was just amazing omg.',
+    date: '2019-05-11T16:12:32.554Z',
+    emotion: 'puke'
+  },
+  {
+    id: 4,
+    author: 'Ilya O"Reilly',
+    comment: 'a film that changed my life, a true masterpiece, post-credit scene was just amazing omg.',
+    date: '2019-05-11T16:12:32.554Z',
+    emotion: 'angry'
+  },
+  {
+    id: 5,
+    author: 'Ilya O"Reilly',
+    comment: 'a film that changed my life, a true masterpiece, post-credit scene was just amazing omg.',
+    date: '2019-05-11T16:12:32.554Z',
+    emotion: 'smile'
+  },
+];
+
+
