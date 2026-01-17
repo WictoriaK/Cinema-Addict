dfc1eccf2fcfc8d8ac0ef1444ecab3e8725fa2f9
Victoria <vitagood.92@list.ru>
2026-01-17 19:35:47 +0300

    Добавит компонентам приватные поля и геттеры

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -5,33 +5,38 @@ import FilmsContainerView from '../view/films-container-view.js';
 import FilmsListView from '../view/films-list-view.js';
 import FilmsListContainerView from '../view/films-list-container.js';
 import ShowMoreBtnView from '../view/show-more-button-view.js';
-// import FilmsCardView from '../view/film-card-view.js';
+import FilmsCardView from '../view/film-card-view.js';
 import FilmPopupView from '../view/film-popup-view.js';
 
 
 export default class BoardPresenter {
-  filmsContainer = new FilmsContainerView();
-  filmsListComponent = new FilmsListView();
-  filmsList = new FilmsListContainerView();
+  #boardContainer = null;
+  #filmsModel = null;
+
+  #boardFilms = [];
+
+  #filmsContainer = new FilmsContainerView();
+  #filmsListComponent = new FilmsListView();
+  #filmsList = new FilmsListContainerView();
 
   init = (boardContainer, filmsModel) => {
-    this.boardContainer = boardContainer;
-    this.filmsModel = filmsModel;
-    this.boardFilms = [...this.filmsModel.getFilms()];
+    this.#boardContainer = boardContainer;
+    this.#filmsModel = filmsModel;
+    this.#boardFilms = [...this.#filmsModel.films];
 
-    render(new SortView(), this.boardContainer);
-    render(this.filmsContainer, this.boardContainer);
-    render(this.filmsListComponent, this.filmsContainer.getElement());
-    render(this.filmsList, this.filmsListComponent.getElement());
+    render(new SortView(), this.#boardContainer);
+    render(this.#filmsContainer, this.#boardContainer);
+    render(this.#filmsListComponent, this.#filmsContainer.element);
+    render(this.#filmsList, this.#filmsListComponent.element);
 
-    // for (let i = 0; i < this.boardFilms.length; i++) {
-    //   render(new FilmsCardView(this.boardFilms[i]), this.filmsList.getElement());
-    // }
+    for (let i = 0; i < this.#boardFilms.length; i++) {
+      render(new FilmsCardView(this.#boardFilms[i]), this.#filmsList.element);
+    }
 
-    render(new FilmPopupView(this.boardFilms[0]), this.boardContainer);
+    // render(new FilmPopupView(this.boardFilms[0]), this.boardContainer);
 
 
-    render(new ShowMoreBtnView(), this.boardContainer);
+    render(new ShowMoreBtnView(), this.#boardContainer);
 
   };
 
--- src/view/show-more-button-view.js
+++ src/view/show-more-button-view.js
@@ -4,19 +4,21 @@ const createShowMoreBtnTemplate = () =>  '<button class="films-list__show-more">
 
 
 export default class ShowMoreBtnView {
-  getTemplate() {
+  #element = null;
+
+  get template() {
     return createShowMoreBtnTemplate();
   }
 
-  getElement() {
-    if (!this.element) {
-      this.element = createElement(this.getTemplate());
+  get element() {
+    if (!this.#element) {
+      this.#element = createElement(this.template);
     }
 
-    return this.element;
+    return this.#element;
   }
 
   removeElement() {
-    this.element = null;
+    this.#element = null;
   }
 }
